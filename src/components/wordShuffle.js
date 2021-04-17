8891aceb05f2fbd5abcbfe98979f1b8d0b733a6a
kayla <kkingston09@gmail.com>
2021-04-17 23:43:29 +0100

    set up state with Word component

--- src/components/wordShuffle.js
+++ src/components/wordShuffle.js
@@ -1,45 +1,90 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import Word from './Word'
 
-const WordShuffle = () => {
-  const [words, setWords] = useState([]);
+class WordShuffle extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      word: undefined,
+      pos: undefined,
+      definition: undefined,
+      error: false
+    }
+  };
 
-  useEffect(() => {
-    requestWords();
-    // eslint-disable-next-line
-  }, [])
+  calIndex(words) {
+    let currentWordIndex = Math.floor(Math.random() * words.length)
+    return currentWordIndex
+  }
 
-  async function requestWords() {
+  requestWords = async () => {
     const resp = await fetch(`http://localhost:3001/api/v1/words`);
-    console.log('resp==>', resp)
+      console.log('resp==>', resp)
     const words = await resp.json();
-    console.log('words==>', words)
-    setWords(words)
-    randomWord(words)
+      console.log('words==>', words)
+
+    this.setState({
+      word: words.word,
+      pos: words.pos,
+      definition: words.definition
+    })
+
+    return words
   }
 
-  const randomWord = () => {
-    const currentWordIndex = Math.floor(Math.random() * words.length)
+  render() {
     return (
-      <Word
-        word={words[currentWordIndex].word}
-        pos={words[currentWordIndex].pos}
-        definition={words[currentWordIndex].definition}
-        key={words[currentWordIndex].id}
-      />
-    )
-  }
+      <div className="word-display">
+        <Word 
+          word={this.state.word}
+          pos={this.state.pos}
+          definition={this.state.definition}
+        />
+      </div>
+    );
+  };
+};
 
-  return (
-    <div className='word-display'>
-      <Word/>
-    </div>
-
-  )
-}
 
 export default WordShuffle;
+
+// const WordShuffle = () => {
+//   const [words, setWords] = useState([]);
+
+//   useEffect(() => {
+//     requestWords();
+//     // eslint-disable-next-line
+//   }, [])
+
+//   async function requestWords() {
+//     const resp = await fetch(`http://localhost:3001/api/v1/words`);
+//     console.log('resp==>', resp)
+//     const words = await resp.json();
+//     console.log('words==>', words)
+//     setWords(words)
+//     randomWord(words)
+//   }
+
+//   const randomWord = () => {
+//     const currentWordIndex = Math.floor(Math.random() * words.length)
+//     return (
+//       <Word
+//         word={words[currentWordIndex].word}
+//         pos={words[currentWordIndex].pos}
+//         definition={words[currentWordIndex].definition}
+//         key={words[currentWordIndex].id}
+//       />
+//     )
+//   }
+
+//   return (
+//     <div className='word-display'>
+//       <Word/>
+//     </div>
+
+//   )
+// }
 
 
 
